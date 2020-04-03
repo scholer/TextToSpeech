4568e4d32003444dbc94f369ba3bfc989863848b
Rasmus Scholer Sorensen <rasmusscholer@gmail.com>
2020-04-03 16:21:17 +0200

    Initial, unfinished support for macOS.

---
+++ simple_tts/tts_mac.py
@@ -0,0 +1,12 @@
+# Copyright 2019, Rasmus Sorensen <rasmusscholer@gmail.com>
+"""
+
+
+"""
+
+import subprocess
+
+
+def speak(text):
+    cmd_args = ['say', text]
+    output = subprocess.check_output(cmd_args, stderr=subprocess.STDOUT, shell=True)
